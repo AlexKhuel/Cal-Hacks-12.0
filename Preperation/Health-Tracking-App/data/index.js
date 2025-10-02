7b28f18afb3974adb9bc61b637059e0eceb5c57f
Joshua Dowd <160157080+AlexKhuel@users.noreply.github.com>
2025-10-02 16:19:21 -0700

    Delete Preperation/Health-Tracking-App directory

--- Preperation/Health-Tracking-App/data/index.js
+++
@@ -1,6 +0,0 @@
-export * from './diet.js';
-export * from './exercise.js';
-export * from './hydration.js';
-export * from './login.js';
-export * from './sleep.js';
-export * from './suggestions.js';
